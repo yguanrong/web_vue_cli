db881076a19cd0b48768200dc4ceb6fd552280ff
Yang Guanrong <0049003252@znv.com>
2019-09-11 17:01:38 +0800

    增加element-UI

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
+import {interceptor} from './utils/ajax'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+Axios.defaults.timeout = 18000; // 延迟时间改为5分钟
+interceptor(Axios);
 
 /* eslint-disable no-new */
 new Vue({
@@ -16,4 +25,4 @@ new Vue({
     // 空的实例放到根组件下，所有的子组件都能调用
     eventBus: new Vue()
   }
-})
+});
